af92b074efd18111e7e4a35607484fe55a06b43b
seraphin2 <93277080+seraphin2@users.noreply.github.com>
2021-10-28 22:57:11 +0700

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -4,7 +4,7 @@ const giftbox = document.getElementById('merrywrap');
 const canvasC = document.getElementById('c');
 
 const config = {
-  birthdate: 'Oct 29, 2021',
+  birthdate: 'Oct 28, 2021',
   name: 'Shannon'
 };
 
